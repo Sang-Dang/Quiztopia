6fc89eac11d4d542f5c837ed0c28d4b4e7d57d85
Sang Dang <sangdase170287@fpt.edu.vn>
2023-04-21 10:33:29 +0700

    create structure

---
+++ src/main/java/com/fpt/assignment/database/util/DBUtils.java
@@ -0,0 +1,32 @@
+package com.fpt.assignment.database.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author User
+ */
+public class DBUtils {
+    private DBUtils() {}
+    
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:sqlserver://myassignment.database.windows.net:1433;"
+                    + "database=QuizDatabase;"
+                    + "user=admins@myassignment;"
+                    + "password=projectAssignment123;"
+                    + "encrypt=true;"
+                    + "trustServerCertificate=false;"
+                    + "hostNameInCertificate=*.database.windows.net;"
+                    + "loginTimeout=30;"
+            );
+        } catch (SQLException ex) {
+            Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
